a241e6705267737f5c90c255c571b4686cc3def7
abdelra0394 <99508372+abdelra0394@users.noreply.github.com>
2024-03-24 19:57:12 +0200

    add models classes

---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Employee
+    {
+        [Column("EmployeeId")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Employee position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string? Position { get; set; }
+
+        [ForeignKey(nameof(Company))]
+        public Guid CompanyId { get; set; }
+        public Company? Company { get; set; }
+    }
+}
