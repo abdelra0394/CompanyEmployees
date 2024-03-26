3fe2978a4c81520a7644882d9fe055491897cb35
abdelra0394 <99508372+abdelra0394@users.noreply.github.com>
2024-03-26 17:47:46 +0200

    adding service project

---
+++ Service/EmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal sealed class EmployeeService : IEmployeeService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public EmployeeService(IRepositoryManager repositoryManager,ILoggerManager loggerManager)
+        {
+            _logger = loggerManager;
+            _repository = repositoryManager;
+        }
+    }
+}
---
+++ Service/ServiceManager.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<ICompanyService> _companyService;
+        private readonly Lazy<IEmployeeService> _employeeService;
+
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager loggerManager)
+        {
+            _companyService = new Lazy<ICompanyService>(() => new CompanyService(repositoryManager, loggerManager));
+            _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repositoryManager, loggerManager));
+        }
+        public IEmployeeService EmployeeService => _employeeService.Value;
+
+        public ICompanyService CompanyService => _companyService.Value;
+    }
+}
