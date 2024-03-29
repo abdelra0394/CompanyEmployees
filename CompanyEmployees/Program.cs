b74b2fae496ddd7d621cdf164badfa1094cb9dbe
abdelra0394 <99508372+abdelra0394@users.noreply.github.com>
2024-03-29 17:54:47 +0200

    adding presentation layer

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -18,7 +18,8 @@ namespace CompanyEmployees
             builder.Services.ConfigureRepositoryManager();
             builder.Services.ConfigureServiceManager();
             builder.Services.ConfigureSqlContext(builder.Configuration);
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
             var app = builder.Build();
 
